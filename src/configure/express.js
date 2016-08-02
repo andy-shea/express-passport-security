ceb9e4e818c2ddfb24835da9d7d7c4f0cd2dab59
Andy Shea <andrew@octahedron.com.au>
2016-08-02 22:10:42 +0100

    Corrected user details returned after successful login

--- src/configure/express.js
+++ src/configure/express.js
@@ -14,10 +14,11 @@ function configureApp(app, loginRoute, logoutRoute, userDetailsExtractor) {
     passport.authenticate('local', (err, user, params) => {
       if (err) return next(err);
       if (!user) return res.status(401).json({error: params ? params.message : 'Invalid login'});
-      req.login(userDetailsExtractor(user), {}, error => {
+        const userDetails = userDetailsExtractor(user);
+        req.login(userDetails, {}, error => {
         if (error) return res.status(500).json({error});
         return res.json({
-          user: {email, name},
+          user: userDetails,
           returnTo: (req.session && req.session.returnTo) ? req.session.returnTo : '/'
         });
       });
